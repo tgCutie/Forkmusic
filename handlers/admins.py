b3c809088f289e379c74530229ca9adefe745cde
tgCutie <91736317+tgCutie@users.noreply.github.com>
2021-10-17 22:45:11 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -21,7 +21,7 @@ async def pause(_, message: Message):
         await message.reply_text("❗ Nothing is playing!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text("▶️ ραυѕє∂!")
+        await message.reply_text("▶️ ᴘᴀᴜsᴇᴅ!")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -36,7 +36,7 @@ async def resume(_, message: Message):
         await message.reply_text("❗ Nothing is paused!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text("⏸ яєѕυмє∂!")
+        await message.reply_text("⏸ ʀᴇsᴜᴍᴇᴅ!")
 
 
 @Client.on_message(command("end") & other_filters)
@@ -44,7 +44,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ иοτнιиg ιѕ ρℓαγιиg!")
+        await message.reply_text("❗ ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ!")
     else:
         try:
             callsmusic.queues.clear(message.chat.id)
@@ -52,7 +52,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("❌ ѕτορ ρℓαγιиg!")
+        await message.reply_text("❌ sᴛᴏᴘ ᴘʟᴀʏɪɴɢ!")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -60,7 +60,7 @@ async def stop(_, message: Message):
 @authorized_users_only
 async def skip(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ иοτнιиg ιѕ ρℓαγιиg το ѕκιρ!")
+        await message.reply_text("❗ ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɴɢ το ѕκιρ!")
     else:
         callsmusic.queues.task_done(message.chat.id)
 
@@ -72,4 +72,4 @@ async def skip(_, message: Message):
                 callsmusic.queues.get(message.chat.id)["file"]
             )
 
-        await message.reply_text("➡️ ѕκιρρє∂ τнє ϲυяяєиτ ѕοиg!")
+        await message.reply_text("➡️ sᴋɪᴘᴘᴇᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ sᴏɴɢ!")
